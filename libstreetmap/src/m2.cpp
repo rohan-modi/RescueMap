6ec7112b36d3e115e5c411dcf9ad07a9209de53d
Kevin Yao <k.yao@mail.utoronto.ca>
2024-03-07 21:47:56 -0500

    Added Draw Street Functionality

--- libstreetmap/src/m2.cpp
+++ libstreetmap/src/m2.cpp
@@ -20,10 +20,12 @@
  */
 
 #include <cmath>
+#include <chrono>
 #include "m1.h"
 #include "m2.h"
 #include "ezgl/application.hpp"
 #include "ezgl/graphics.hpp"
+#include <thread>
 
 // Declare global variables
 struct Intersection_data {
@@ -31,6 +33,7 @@ struct Intersection_data {
    std::string name;
 };
 std::vector<Intersection_data> intersections;
+extern std::vector<std::vector<int>> streetSegments;
 
 struct Map_bounds {
    double max_lat;
@@ -45,6 +48,12 @@ float cos_latavg;
 // Declare helper functions
 void draw_main_canvas (ezgl::renderer *g);
 void initializeIntersections();
+void draw_intersections(ezgl::renderer *g);
+void draw_streets(ezgl::renderer *g);
+void draw_POI(ezgl::renderer *g);
+ezgl::point2d latlon_to_point(LatLon position);
+void draw_features(ezgl::renderer *g);
+void set_feature_color(ezgl::renderer *g, int feature_id);
 
 float x_from_lon(float lon);
 float y_from_lat(float lat);
@@ -83,26 +92,63 @@ void drawMap() {
    // Run the application
    application.run(nullptr, nullptr, nullptr, nullptr);
 
-} // void drawMap()
+}
 
 
 void draw_main_canvas (ezgl::renderer *g) {
-   g->set_color(ezgl::BLACK);
-   g->set_line_width(10);
-   g->draw_rectangle({x_from_lon(mapBounds.min_lon), y_from_lat(mapBounds.min_lat)},
-                     {x_from_lon(mapBounds.max_lon), y_from_lat(mapBounds.max_lat)});
+   draw_intersections(g);
+   draw_streets(g);
+   draw_features(g);
+}
 
+void draw_intersections(ezgl::renderer *g){
+   auto startTime = std::chrono::high_resolution_clock::now();
+   g->set_color(ezgl::RED);
    for (IntersectionIdx inter_id = 0; inter_id < intersections.size(); inter_id++) {
       float x = x_from_lon(intersections[inter_id].position.longitude());
       float y = y_from_lat(intersections[inter_id].position.latitude());
 
-      float width = 100;
+      float width = 5;
       float height = width;
 
       g->fill_rectangle({x, y}, {x + width, y + height});
 
    }
+   auto currTime = std::chrono::high_resolution_clock::now();
+   auto wallClock = std::chrono::duration_cast<std::chrono::duration<double>>(currTime - startTime);
+   std::cout << "draw_intersections took " << wallClock.count() <<" seconds" << std::endl;
+}
 
+void draw_streets(ezgl::renderer *g){
+   auto startTime = std::chrono::high_resolution_clock::now();
+
+   g->set_line_width(1);
+   g->set_color(187, 187, 187);
+
+
+   for(int segment_id = 0; segment_id < getNumStreetSegments(); segment_id++){
+      if(getStreetSegmentInfo(segment_id).numCurvePoints > 0){
+         LatLon point1 = getIntersectionPosition(getStreetSegmentInfo(segment_id).from);
+         LatLon point2;
+         for(int part_id = 0; part_id < getStreetSegmentInfo(segment_id).numCurvePoints; part_id++){
+            point2 = getStreetSegmentCurvePoint(part_id, segment_id);
+            
+            g->draw_line(latlon_to_point(point1), latlon_to_point(point2));
+            point1 = point2;
+         }
+         point2 = getIntersectionPosition(getStreetSegmentInfo(segment_id).to);
+         g->draw_line(latlon_to_point(point1), latlon_to_point(point2));
+      } else{
+         LatLon point1 = getIntersectionPosition(getStreetSegmentInfo(segment_id).from);
+         LatLon point2 = getIntersectionPosition(getStreetSegmentInfo(segment_id).to);
+
+         g->draw_line(latlon_to_point(point1), latlon_to_point(point2));
+      }
+   }
+
+   auto currTime = std::chrono::high_resolution_clock::now();
+   auto wallClock = std::chrono::duration_cast<std::chrono::duration<double>>(currTime - startTime);
+   std::cout << "draw_streets took " << wallClock.count() <<" seconds" << std::endl;
 }
 
 
@@ -128,8 +174,6 @@ void initializeIntersections() {
 
 }
 
-
-
 float x_from_lon(float lon) {
    return kEarthRadiusInMeters * kDegreeToRadian * lon * cos_latavg;
 }
@@ -144,4 +188,11 @@ float lon_from_x(float x) {
 
 float lat_from_y(float y) {
    return y / kEarthRadiusInMeters / kDegreeToRadian;
+}
+
+ezgl::point2d latlon_to_point(LatLon position){
+   float x = kEarthRadiusInMeters * kDegreeToRadian * position.longitude() * cos_latavg;
+   float y = kEarthRadiusInMeters * kDegreeToRadian * position.latitude();
+
+   return(ezgl::point2d(x,y));
 }
